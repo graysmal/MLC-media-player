f467e0bb8388120c5a1a278db2c1fa327f19db72
graysmal <128539509+graysmal@users.noreply.github.com>
2024-04-03 15:32:59 -0500

    Project Setup @ FT

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,26 @@
 #include <wx/wx.h>
+#include <iostream>
 
 class App : public wxApp {
 public:
     bool OnInit() {
-        wxFrame* window = new wxFrame(NULL, wxID_ANY, "GUI Test", wxDefaultPosition, wxSize(600, 400));
+        wxFrame* window = new wxFrame(NULL, wxID_ANY, "MLC media player", wxDefaultPosition, wxSize(1280, 720));
         wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
-        wxStaticText* text = new wxStaticText(window, wxID_ANY, "Well Done!\nEverything seems to be working",
+        wxStaticText* text = new wxStaticText(window, wxID_ANY, "Test Message",
             wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
+
+        wxButton* button = new wxButton(window, wxID_ANY, "Test Button", 
+            wxPoint(100, 100), wxSize(100, 100));
+
         text->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
-        sizer->Add(text, 1, wxALIGN_CENTER);
+        sizer->Add(button, 1, wxALIGN_CENTER);
         window->SetSizer(sizer);
         window->Show();
         return true;
+    }
+
+    void printHelloWorld() {
+        std::cout << "Hello World!";
     }
 };
 
