979c7210ac47883dd3f2e00eb5c8b8da5dcb8180
graysmal <128539509+graysmal@users.noreply.github.com>
2024-04-11 15:31:46 -0500

    Created base panels and menubar

--- Main.cpp
+++ Main.cpp
@@ -5,23 +5,58 @@ class App : public wxApp {
 public:
     bool OnInit() {
         wxFrame* window = new wxFrame(NULL, wxID_ANY, "MLC media player", wxDefaultPosition, wxSize(1280, 720));
-        wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
-        wxStaticText* text = new wxStaticText(window, wxID_ANY, "Test Message",
-            wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
+        wxBoxSizer* app_sizer = new wxBoxSizer(wxVERTICAL);
+        
+        // MENU BAR
+        wxMenuBar* menu_bar = new wxMenuBar(wxHORIZONTAL);
 
-        wxButton* button = new wxButton(window, wxID_ANY, "Test Button", 
-            wxPoint(100, 100), wxSize(100, 100));
+        /// FILE
+        wxMenu* file_menu = new wxMenu();
+        file_menu->Append(wxID_ANY, "open");
+        menu_bar->Append(file_menu, "file");
 
-        text->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
-        sizer->Add(button, 1, wxALIGN_CENTER);
-        window->SetSizer(sizer);
+        /// EDIT
+        wxMenu* edit_menu = new wxMenu();
+        edit_menu->Append(wxID_ANY, "preferences");
+        menu_bar->Append(edit_menu, "edit");
+
+        wxPanel* preview_panel = new wxPanel(window, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+        preview_panel->SetMinSize(wxSize(200, 200));
+        wxBoxSizer* preview_sizer = new wxBoxSizer(wxVERTICAL);
+        preview_panel->SetSizer(preview_sizer);
+        preview_sizer->AddStretchSpacer();
+        preview_panel->SetBackgroundColour("black");
+
+
+        wxPanel* playback_tools_panel = new wxPanel(window, wxID_ANY, wxDefaultPosition, wxDefaultSize);
+        wxBoxSizer* playback_tools_sizer = new wxBoxSizer(wxHORIZONTAL);
+        wxButton* play_button = new wxButton(playback_tools_panel, wxID_ANY, "play", wxDefaultPosition, wxDefaultSize);
+        wxButton* stop_button = new wxButton(playback_tools_panel, wxID_ANY, "stop", wxDefaultPosition, wxDefaultSize);
+
+        playback_tools_panel->SetSizer(playback_tools_sizer);
+        playback_tools_sizer->Add(play_button, 0);
+        playback_tools_sizer->Add(stop_button, 0);
+
+        app_sizer->Add(preview_panel, 1, wxEXPAND | wxALL, 5);
+        app_sizer->Add(playback_tools_panel, 0, wxALIGN_BOTTOM | wxALL, 5);
+        
+        window->SetMenuBar(menu_bar);
+        window->SetSizer(app_sizer);
         window->Show();
         return true;
     }
 
+
+
+
+
     void printHelloWorld() {
         std::cout << "Hello World!";
     }
+
+    //wxStaticText* text = new wxStaticText(window, wxID_ANY, "Test Message",
+        //    wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE_HORIZONTAL);
+        //text->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
 };
 
 wxIMPLEMENT_APP(App);
